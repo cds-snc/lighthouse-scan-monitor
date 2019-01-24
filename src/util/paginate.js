2f0e79747f1899642bcc1ffeda8ef46d9536ddcd
Tim Arney <tim@line37.com>
2019-01-24 15:13:49 -0500

    fix css

--- src/util/paginate.js
+++ src/util/paginate.js
@@ -1,4 +1,4 @@
-export const paginate = async (firebase, nextPage = false, limit = 100) => {
+export const paginate = async (firebase, nextPage = false, limit = 20) => {
   const db = firebase.firestore();
 
   const baseQuery = db
